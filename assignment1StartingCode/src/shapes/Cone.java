fd11906cd3d4828a4e6a06281413d0e749b2e42d
limidotu <lashtabeha.dmytro@gmail.com>
2026-05-27 10:55:11 -0600

    comparator

--- assignment1StartingCode/src/shapes/Cone.java
+++ assignment1StartingCode/src/shapes/Cone.java
@@ -1,6 +1,17 @@
 package shapes;
 
-public class Cone
+import appDomain.Shape;
+
+public class Cone extends Shape
 {
+    private double radius;
+   @Override
+   public double calcBaseArea() {
+       return Math.PI * radius;
+   }
+   @Override
+   public double calcVolume() {
+        return 1/3 * Math.PI * radius * radius * this.getHeight();
+   }
 
 }
--- assignment1StartingCode/src/shapes/Cylinder.java
+++ assignment1StartingCode/src/shapes/Cylinder.java
@@ -1,6 +1,19 @@
 package shapes;
 
-public class Cylinder
+import appDomain.Shape;
+
+public class Cylinder extends Shape
 {
+    private double radius;
+
+    @Override
+    public double calcBaseArea() {
+       
+        return Math.PI * radius * radius;
+    }
+    @Override
+    public double calcVolume() {
+        return this.calcBaseArea() * this.getHeight();
+    }
 
 }
