c6b81e3a408d1b557bcd207bca2f045581571d5d
limidotu <lashtabeha.dmytro@gmail.com>
2026-05-27 09:23:44 -0600

    Octagonal Prism

--- assignment1StartingCode/src/shapes/OctagonalPrism.java
+++ assignment1StartingCode/src/shapes/OctagonalPrism.java
@@ -1,6 +1,31 @@
 package shapes;
 
-public class OctagonalPrism
-{
 
+
+/**
+ * Represents a Octagonal Prism.
+ * 
+ * @author Dmytro
+ */
+
+public class OctagonalPrism extends appDomain.Prism
+{
+    /**
+     * Calculates the base area.
+     * @return the base area.
+     */
+    public double calcBaseArea() {
+        double side =  this.getSide();
+        // 2*(1 + sqrt(2))side^2
+        return 2 * (1 + Math.sqrt(2) * side * side);
+    }
+
+    /**
+     * Calculates the volume.
+     * @return the volume.
+     */
+    public double calcVolume() {
+        // A*h
+        return this.calcBaseArea() * this.getHeight();
+    }
 }
