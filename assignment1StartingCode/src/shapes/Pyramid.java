523d9d8bf45ad8485fbe75e5948f042c394e479a
limidotu <lashtabeha.dmytro@gmail.com>
2026-05-28 11:04:32 -0600

    All shapes are done

--- assignment1StartingCode/src/shapes/Pyramid.java
+++ assignment1StartingCode/src/shapes/Pyramid.java
@@ -6,11 +6,20 @@ public class Pyramid extends Shape
 {
     private double side;
 
-     @Override
+    /**
+     * Calculates the base area of the pyramid.
+     * @return the base area (side squared).
+     */
+    @Override
     public double calcBaseArea() {
        
         return side * side;
     }
+
+    /**
+     * Calculates the volume of the pyramid.
+     * @return the volume (1/3 * base area * height).
+     */
     @Override
     public double calcVolume() {
         return (1/3) * this.calcBaseArea() * getHeight();
